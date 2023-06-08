3bc021802eac8e2723db248b9065473029343faa
scorstje <s.corstjens@outlook.com>
2023-06-08 16:34:46 +0200

    07/06/2023 - Audio translation finished - - Main analysis finished -

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import McsPy.McsData
 from McsPy import ureg, Q_
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.io.wavfile import write
+
+from sklearn.mixture import GaussianMixture
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 def plot_analog_stream_channel(analog_stream, channel_idx, from_in_s=0, to_in_s=None, show=True):
@@ -45,7 +48,7 @@ def plot_analog_stream_channel(analog_stream, channel_idx, from_in_s=0, to_in_s=
 
     # construct the plot
     _ = plt.figure(figsize=(16, 5))
-    _ = plt.plot(time_in_sec, signal_in_uV)
+    _ = plt.plot(time_in_sec, signal_in_uV, linewidth=0.1)
     _ = plt.xlabel('Time (%s)' % ureg.s)
     _ = plt.ylabel('Voltage (%s)' % ureg.uV)
     _ = plt.title('Channel %s' % channel_info.info['Label'])
@@ -53,23 +56,199 @@ def plot_analog_stream_channel(analog_stream, channel_idx, from_in_s=0, to_in_s=
         plt.show()
 
 
-Rate = 10000
+def detect_threshold_crossings(signal, fs, threshold, dead_time):
+    """
+    Detect threshold crossings in a signal with dead time and return them as an array
+
+    The signal transitions from a sample above the threshold to a sample below the threshold for a detection and
+    the last detection has to be more than dead_time apart from the current one.
+
+    :param signal: The signal as a 1-dimensional numpy array
+    :param fs: The sampling frequency in Hz
+    :param threshold: The threshold for the signal
+    :param dead_time: The dead time in seconds.
+    """
+    dead_time_idx = dead_time * fs
+    threshold_crossings = np.diff((signal <= threshold).astype(int) > 0).nonzero()[0]
+    distance_sufficient = np.insert(np.diff(threshold_crossings) >= dead_time_idx, 0, True)
+    while not np.all(distance_sufficient):
+        # repeatedly remove all threshold crossings that violate the dead_time
+        threshold_crossings = threshold_crossings[distance_sufficient]
+        distance_sufficient = np.insert(np.diff(threshold_crossings) >= dead_time_idx, 0, True)
+    return threshold_crossings
+
+
+def get_next_minimum(signal, index, max_samples_to_search):
+    """
+    Returns the index of the next minimum in the signal after an index
+
+    :param signal: The signal as a 1-dimensional numpy array
+    :param index: The scalar index
+    :param max_samples_to_search: The number of samples to search for a minimum after the index
+    """
+    search_end_idx = min(index + max_samples_to_search, signal.shape[0])
+    min_idx = np.argmin(signal[index:search_end_idx])
+    return index + min_idx
+
+
+def align_to_minimum(signal, fs, threshold_crossings, search_range):
+    """
+    Returns the index of the next negative spike peak for all threshold crossings
+
+    :param signal: The signal as a 1-dimensional numpy array
+    :param fs: The sampling frequency in Hz
+    :param threshold_crossings: The array of indices where the signal crossed the detection threshold
+    :param search_range: The maximum duration in seconds to search for the minimum after each crossing
+    """
+    search_end = int(search_range * fs)
+    aligned_spikes = [get_next_minimum(signal, t, search_end) for t in threshold_crossings]
+    return np.array(aligned_spikes)
+
+
+def extract_waveforms(signal, fs, spikes_idx, pre, post):
+    """
+    Extract spike waveforms as signal cutouts around each spike index as a spikes x samples numpy array
+
+    :param signal: The signal as a 1-dimensional numpy array
+    :param fs: The sampling frequency in Hz
+    :param spikes_idx: The sample index of all spikes as a 1-dim numpy array
+    :param pre: The duration of the cutout before the spike in seconds
+    :param post: The duration of the cutout after the spike in seconds
+    """
+    cutouts = []
+    pre_idx = int(pre * fs)
+    post_idx = int(post * fs)
+    for index in spikes_idx:
+        if index - pre_idx >= 0 and index + post_idx <= signal.shape[0]:
+            cutout = signal[(index - pre_idx):(index + post_idx)]
+            cutouts.append(cutout)
+    return np.stack(cutouts)
+
+
+def plot_waveforms(cutouts, fs, pre, post, n=100, color='k', show=True):
+    """
+    Plot an overlay of spike cutouts
+
+    :param cutouts: A spikes x samples array of cutouts
+    :param fs: The sampling frequency in Hz
+    :param pre: The duration of the cutout before the spike in seconds
+    :param post: The duration of the cutout after the spike in seconds
+    :param n: The number of cutouts to plot, or None to plot all. Default: 100
+    :param color: The line color as a pyplot line/marker style. Default: 'k'=black
+    :param show: Set this to False to disable showing the plot. Default: True
+    """
+    if n is None:
+        n = cutouts.shape[0]
+    n = min(n, cutouts.shape[0])
+    time_in_us = np.arange(-pre * 1000, post * 1000, 1e3 / fs)
+    if show:
+        _ = plt.figure(figsize=(12, 6))
+
+    for i in range(n):
+        _ = plt.plot(time_in_us, cutouts[i,] * 1e6, color, linewidth=0.8, alpha=0.3)
+        _ = plt.xlabel('Time (%s)' % ureg.ms)
+        _ = plt.ylabel('Voltage (%s)' % ureg.uV)
+        _ = plt.title('Cutouts')
+
+    if show:
+        plt.show()
+
+
+Rate = 50000
 
 rootDir = os.path.dirname(os.path.abspath(__file__))
-D5data = f'{rootDir}\\wD6_D18_hc.h5'
+D5data = f'{rootDir}\\Cx_50kHz.h5'
 file = McsPy.McsData.RawData(D5data)
-
 electrode_stream = file.recordings[0].analog_streams[0]
 
-# get the signal
 ids = [c.channel_id for c in electrode_stream.channel_infos.values()]
-signal = electrode_stream.get_channel_in_range(ids[7], 0, 499999)
+channel_id = ids[4]
+info = electrode_stream.channel_infos[channel_id].info
+print("Bandwidth: %s - %s Hz" % (info['HighPassFilterCutOffFrequency'], info['LowPassFilterCutOffFrequency']))
 
-signalArray = np.array(signal[0])
-rate = 10000
-write('Amplitude.wav', rate, signalArray * 10000)
+signal = electrode_stream.get_channel_in_range(channel_id, 0, electrode_stream.channel_data.shape[1])[0]
+noise_mad = np.median(np.absolute(signal)) / 0.6745
+print('Noise Estimate by MAD Estimator : {0:g} V'.format(noise_mad))
+spike_threshold = -7 * noise_mad
 
-x = np.arange(1, 5, 0.001)
-y = list()
+timeStart = 0
+timeStop = 30
 
-plot_analog_stream_channel(electrode_stream, 7, from_in_s=0, to_in_s=300)
+fs = int(electrode_stream.channel_infos[channel_id].sampling_frequency.magnitude)
+crossings = detect_threshold_crossings(signal, fs, spike_threshold, 0.003)
+spks = align_to_minimum(signal, fs, crossings, 0.002)
+
+timestamps = spks / fs
+range_in_s = (timeStart, timeStop)
+spikes_in_range = timestamps[(timestamps >= range_in_s[0]) & (timestamps <= range_in_s[1])]
+
+# plot_analog_stream_channel(electrode_stream, 4, from_in_s=timeStart, to_in_s=timeStop, show=False)
+# _ = plt.plot(spikes_in_range, [spike_threshold * 1e6] * spikes_in_range.shape[0], 'ro', ms=0.4)
+# plt.show()
+
+pre = 0.001  # 1 ms
+post = 0.002  # 2 ms
+cutouts = extract_waveforms(signal, fs, spks, pre, post)
+print("Cutout array shape: " + str(cutouts.shape))  # number of spikes x number of samples
+
+plot_waveforms(cutouts, fs, pre, post, n=500)
+
+min_amplitude = np.amin(cutouts, axis=1)
+max_amplitude = np.amax(cutouts, axis=1)
+
+_ = plt.figure(figsize=(8, 8))
+_ = plt.plot(min_amplitude * 1e6, max_amplitude * 1e6, '.')
+_ = plt.xlabel('Min. Amplitude (%s)' % ureg.uV)
+_ = plt.ylabel('Max. Amplitude (%s)' % ureg.uV)
+_ = plt.title('Min/Max Spike Amplitudes')
+
+plt.show()
+
+scaler = StandardScaler()
+scaled_cutouts = scaler.fit_transform(cutouts)
+
+pca = PCA()
+pca.fit(scaled_cutouts)
+print(pca.explained_variance_ratio_)
+
+pca.n_components = 3
+transformed_3d = pca.fit_transform(scaled_cutouts)
+
+_ = plt.figure(figsize=(15, 5))
+_ = plt.subplot(1, 3, 1)
+_ = plt.plot(transformed_3d[:, 0], transformed_3d[:, 1], '.')
+_ = plt.xlabel('Principal Component 1')
+_ = plt.ylabel('Principal Component 2')
+_ = plt.title('PC1 vs PC2')
+_ = plt.subplot(1, 3, 2)
+_ = plt.plot(transformed_3d[:, 0], transformed_3d[:, 2], '.')
+_ = plt.xlabel('Principal Component 1')
+_ = plt.ylabel('Principal Component 3')
+_ = plt.title('PC1 vs PC3')
+_ = plt.subplot(1, 3, 3)
+_ = plt.plot(transformed_3d[:, 1], transformed_3d[:, 2], '.')
+_ = plt.xlabel('Principal Component 2')
+_ = plt.ylabel('Principal Component 3')
+_ = plt.title('PC2 vs PC3')
+plt.show()
+
+n_components = 2
+gmm = GaussianMixture(n_components=n_components, n_init=10)
+labels = gmm.fit_predict(transformed_3d)
+
+_ = plt.figure(figsize=(8, 8))
+for i in range(n_components):
+    idx = labels == i
+    _ = plt.plot(transformed_3d[idx, 0], transformed_3d[idx, 1], '.')
+    _ = plt.title('Cluster assignments by a GMM')
+    _ = plt.xlabel('Principal Component 1')
+    _ = plt.ylabel('Principal Component 2')
+    _ = plt.axis('tight')
+plt.show()
+
+_ = plt.figure(figsize=(8,8))
+for i in range(n_components):
+    idx = labels == i
+    color = plt.rcParams['axes.prop_cycle'].by_key()['color'][i]
+    plot_waveforms(cutouts[idx,:], fs, pre, post, n=100, color=color, show=False)
+plt.show()
